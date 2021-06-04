3b2de9a18bb1c7f559fba6c6ac927e77d42701d5
Brandon Robertson <brandonrobertson23@gmail.com>
2021-06-04 14:52:29 +1000

    Added some PrintLine's

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,21 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    PrintLine(TEXT("I am an UE4 GOD!"));
+    PrintLine(TEXT("...seriously"));
+    HiddenWord = TEXT("cake");
+    PrintLine(TEXT("Guess the word"));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-
+    ClearScreen();    
+    if (Input == TEXT("let me win") || Input == HiddenWord)
+    {
+        PrintLine(TEXT("you win da game"));
+    }
+    else
+    {
+        PrintLine(TEXT("You suck ass bro, try again"));
+    }
 }
