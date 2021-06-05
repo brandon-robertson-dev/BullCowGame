ed51bfb5500a161536cb71585cc7c76a5fc4acd1
Brandon Robertson <brandonrobertson23@gmail.com>
2021-06-05 13:03:33 +1000

    Kinda works so far but need to add score and random words

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,21 +4,68 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
-    PrintLine(TEXT("I am an UE4 GOD!"));
-    PrintLine(TEXT("...seriously"));
-    HiddenWord = TEXT("cake");
-    PrintLine(TEXT("Guess the word"));
+
+    SetupGame();
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-    ClearScreen();    
-    if (Input == TEXT("let me win") || Input == HiddenWord)
+    if (bGameOver)
     {
-        PrintLine(TEXT("you win da game"));
+        ClearScreen();
+        SetupGame();
     }
     else
     {
-        PrintLine(TEXT("You suck ass bro, try again"));
+        CheckGuess(Input);
+    }
+}
+
+void UBullCowCartridge::SetupGame()
+{
+    PrintLine(TEXT("Welcome to the game"));
+    HiddenWord = TEXT("cake");
+    Lives = 5;
+    bGameOver = false;
+    // PrintLine(TEXT(""));
+    PrintLine(FString::Printf(TEXT("Guess the %i letter long word"), HiddenWord.Len()));
+    PrintLine(FString::Printf(TEXT("You have %i lives left"), Lives));
+    PrintLine(TEXT("Type a word and press enter to guess"));
+}
+
+void UBullCowCartridge::GameOver()
+{
+    bGameOver = true;
+    PrintLine(TEXT("\nPress enter to play again"));
+}
+
+void UBullCowCartridge::CheckGuess(const FString& Guess)
+{
+    ClearScreen();
+
+    if (Guess == HiddenWord)
+    {
+        PrintLine(TEXT("You have won"));
+        GameOver();
+        return;
+    }
+    
+    if (Guess.Len() != HiddenWord.Len())
+    {
+        PrintLine(FString::Printf(TEXT("Guess the %i letter long word"), HiddenWord.Len()));
+        PrintLine(FString::Printf(TEXT("You have %i lives left"), Lives));
+        return;
+    }
+
+    PrintLine(TEXT("You lost a life, try again"));
+    --Lives;
+
+    if (Lives <= 0)
+    {
+        ClearScreen();
+        PrintLine(TEXT("No lives left"));
+        PrintLine(FString::Printf(TEXT("The hidden word was %s"), *HiddenWord));
+        GameOver();
+        return;
     }
 }
